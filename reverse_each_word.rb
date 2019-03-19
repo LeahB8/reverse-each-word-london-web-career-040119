004c28b6b2938ba741d5f1620c8f92798b4570ba
Leah Boyd <leahboyd08@gmail.com>
2019-03-19 15:54:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ def reverse_each_word_with_each(string)
 end
 
 
-def reverse_each_word(array)
+def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
   array.collect do |string| 
