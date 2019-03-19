7aa552ff8573718a058928383c498a3c611df290
Leah Boyd <leahboyd08@gmail.com>
2019-03-19 14:58:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,8 @@ def reverse_each_word(string)
   string.split(" ")
   array.reverse_each {|word| print word, " "}
 end
+
+def reverse_each_word(string)
+  string.split(" ")
+  array.collect_each {|word| print word, " "}
+end
